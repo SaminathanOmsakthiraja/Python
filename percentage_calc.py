fb923b19e734727dd80ea074b9b7838fb36a0ec3
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-06-28 21:22:33 +0530

    Calculate percentage of marks

---
+++ percentage_calc.py
@@ -0,0 +1,7 @@
+n = int(input("Enter the number of subjects : "))
+lst = []
+for i in range(n):
+    lst.append(int(input()))
+value = sum(lst)
+total = n*100;
+print('The percentage of the marks is ', (value / total) * 100,' % ')
