5eacfaf69a42e3582bc328ac553b6fc95dc7d735
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-06-29 19:10:54 +0530

    Calculate the Simple interest

--- Python Basics/Simple_interest.py
+++ Python Basics/Simple_interest.py
@@ -2,4 +2,4 @@ p = eval(input("Enter the Principal amt : "))
 r = eval(input("Enter the Rate of interest : "))
 t = eval(input("Enter the Time : "))
 interest = (p * r * t)/100
-print(interest)
+print('The Simple interest is ',interest)
