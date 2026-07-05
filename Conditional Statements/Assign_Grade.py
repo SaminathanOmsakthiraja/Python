ef753bda7552e17238b9c73547b2938ea4f902c5
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-07-05 20:55:50 +0530

    Assign the Grade for the students according to their mark

---
+++ Conditional Statements/Assign_Grade.py
@@ -0,0 +1,11 @@
+n = int(input("Enter the total : "))
+if n >= 90:
+    print("Grade A")
+elif n >= 75:
+    print('Grade B')
+elif n >= 50:
+    print('Grade C')
+elif n >= 35:
+    print("Grade D")
+else :
+    print("Grade F")
