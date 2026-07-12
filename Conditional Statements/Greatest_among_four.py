9f94bae8adca0d951c0ff5c678981f0e8550283e
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-07-12 21:38:09 +0530

    Greatest among entered four numbers

---
+++ Conditional Statements/Greatest_among_four.py
@@ -0,0 +1,5 @@
+n1 = int(input('Enter the number 1 : '))
+n2 = int(input('Enter the number 2 : '))
+n3 = int(input('Enter the number 3 : '))
+n4 = int(input('Enter the number 4 : '))
+print('The maximum among four is',max(n1,n2,n3,n4))
