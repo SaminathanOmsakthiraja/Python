a6217d0dd4549129b9e84942f7d0a9004450e9fd
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-07-12 21:51:38 +0530

    Check wheather the entered triangele is scalene or not

---
+++ Conditional Statements/check_scalen_triangle.py
@@ -0,0 +1,7 @@
+a1 = int(input('Enter the angle 1 : '))
+a2 = int(input('Enter the angle 2 : '))
+a3 = int(input('Enter the angle 3 : '))
+if (a1 != a2 and a2 != a3 and a3 != a1) and a1 + a2 + a3 == 180:
+    print("This is the Scalene Triangle")
+else:
+    print("This is not the Scalene Triangle")
